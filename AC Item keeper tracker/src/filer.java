54012887c966d7b09a1fd825eb5e13529a7ec485
Mark Andrews <mandrews18@knights.ucf.edu>
2013-10-20 22:18:29 -0400

    Small amount to open and read file

--- AC Item keeper tracker/src/filer.java
+++ AC Item keeper tracker/src/filer.java
@@ -1,4 +1,31 @@
+import java.io.*;
+import java.util.*;
 
 public class filer {
-
+	private File input;
+	private Scanner fileReader;
+	private HashSet<String> itemList = new HashSet<String>();
+	
+	public filer() throws IOException{
+		
+		try
+		{
+			input = new File("list.txt");
+			fileReader = new Scanner(input);
+		}
+		catch (FileNotFoundException ex)
+		{
+			if(!input.createNewFile()){
+				System.out.println("Error opening file");
+				System.exit(-1);
+			}
+			fileReader = new Scanner(input);
+		}
+		while(fileReader.hasNext())
+			itemList.add(fileReader.nextLine());
+	}
+	
+	public void searchList(String item){
+		
+	}
 }
