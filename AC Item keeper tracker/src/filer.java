ae6ccc726a22aa213c84c8fb3d9a1ff5683b8d67
Mark Andrews <mandrews18@knights.ucf.edu>
2013-10-25 09:48:57 -0400

    Added functionality to read file into a list stored in a HashMap with the ability to store specific fashion/ set information in the future. Should be able to search and display where an item is or should be in a list relative to a few entries before and after in O(1) time.

--- AC Item keeper tracker/src/filer.java
+++ AC Item keeper tracker/src/filer.java
@@ -4,13 +4,13 @@ import java.util.*;
 public class filer {
 	private File input;
 	private Scanner fileReader;
-	private HashSet<String> itemList = new HashSet<String>();
+	private HashSet<Entry> itemList = new HashSet<Entry>();
 	
 	public filer() throws IOException{
 		
 		try
 		{
-			input = new File("list.txt");
+			input = new File("index.txt");
 			fileReader = new Scanner(input);
 		}
 		catch (FileNotFoundException ex)
@@ -23,9 +23,50 @@ public class filer {
 		}
 		while(fileReader.hasNext())
 			itemList.add(fileReader.nextLine());
+		populateList();
+	}
+	
+	private void populateList(){
+		Entry prev = null;
+		Entry current = null;
+		
+		while(fileReader.hasNext()){
+			current = new Entry(fileReader.nextLine(), prev);
+			
+			//insert entry it isn't a duplicate
+			if(!itemList.contains(current)){
+				itemList.add(current);
+				if( prev != null )
+					prev.addNext(current);
+				prev = current;
+			}
+		}
 	}
 	
 	public void searchList(String item){
 		
+	}
+}
+
+class Entry implements Comparable<Entry>{
+	public String displayName;
+	public String searchName;
+	private int set = 0;
+	private int type = 0;
+	public Entry next;
+	public Entry prev;
+	
+	public Entry(String a, Entry prev){
+		displayName = a;
+		searchName = a.toLowerCase();
+		this.prev = prev;
+	}
+	
+	public int compareTo(Entry a){
+		return searchName.compareTo(a.searchName);
+	}
+	
+	public void addNext(Entry a){
+		next = a;
 	}
 }
